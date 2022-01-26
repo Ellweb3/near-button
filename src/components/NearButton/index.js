eb155c48114b41681f03bf3c701bb473bc83c68b
Ellweb3 <95847507+Ellweb3@users.noreply.github.com>
2022-01-26 09:17:06 +0300

    change contract call

--- src/components/NearButton/index.js
+++ src/components/NearButton/index.js
@@ -1,28 +1,17 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import * as nearAPI from 'near-api-js';
-const { providers, utils, keyStores, KeyPair } = nearAPI
+import { parseContract } from 'near-contract-parser';
 import getConfig from './config.js';
 import { Button, Text, View, StyleSheet } from 'react-native'
+const { keyStores, KeyPair } = nearAPI
 
 
 const NearButton = (props) => {
-	const {iconIn, iconOut, signInTitle, signOutTitle, backgroundColor, onConnect, netType, contractName } = props
+	const { iconIn, iconOut, signInTitle, signOutTitle, backgroundColor, onConnect, netType, contractName, contractMethods, addMethod } = props
 	const [connect, setConnect] = useState(null)
-	const [nfts, setNfts] = useState([])
-	const [user, setUser] = useState(null)
-	const [action, setAction] = useState(null)
 
-	
-if (connect && connect.currentUser && !action) {
-	let balance = +connect.currentUser.balance
-	let userId = connect.currentUser.accountId.toString()
-	console.log(userId);
-	onConnect(balance,userId)
-	setAction(1)
-}
 	useEffect(() => {
-
 		const fetchData = async () => {
 			const configParametr = {
 				env: process.env.NODE_ENV || netType,
@@ -30,17 +19,10 @@ if (connect && connect.currentUser && !action) {
 				netType: netType
 			}
 			const nearConfig = getConfig(configParametr);
-			console.log('conf', process.env.NODE_ENV);
-			// const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
 			const keyStore = new keyStores.InMemoryKeyStore()
 			const accessKey = KeyPair.fromRandom('ed25519')
-			
-			console.log('access', accessKey.toString());
 			const keyPair = KeyPair.fromString(accessKey.toString())
 			await keyStore.setKey(netType, contractName, keyPair)
-
-
-
 
 			const near = await nearAPI.connect({ keyStore, ...nearConfig });
 			const walletConnection = new nearAPI.WalletConnection(near);
@@ -52,83 +34,101 @@ if (connect && connect.currentUser && !action) {
 					balance: (await walletConnection.account().state()).amount,
 				};
 			}
+
 			const contract = await new nearAPI.Contract(
 				walletConnection.account(),
 				nearConfig.contractName,
 				{
-					viewMethods: ['getAllNFTsByOwner'],
-					changeMethods: ['mintNewNFT',],
+					viewMethods: [],
+					changeMethods: [],
 					sender: walletConnection.getAccountId(),
 				}
 			);
-			
-			// (async () => {
-			// 	const account_id = 'one.nfts.testnet';
-			// 	const { code_base64 } = await near.connection.provider.query({
-			// 	  account_id,
-			// 	  finality: 'final',
-			// 	  request_type: 'view_code',
-			// 	});
-			  
-			// 	console.log("parse contract",parseContract(code_base64));
-			//   })();
+			if (contractMethods) {
+				if (contractMethods.length == 0) {
+					(async () => {
+						const account_id = nearConfig.contractName;
+						const { code_base64 } = await near.connection.provider.query({
+							account_id,
+							finality: 'final',
+							request_type: 'view_code',
+						});
+						const newMethods = parseContract(code_base64)
+						newMethods.methodNames.map((method) => {
+							addMethod(method)
+						})
+					})();
+				}
 
-			setConnect({ contract, currentUser, nearConfig, walletConnection, near })
-			if (currentUser) {
-	console.log("accoundID", currentUser.accountId);
-	setUser(+currentUser.balance, currentUser.accountId )
-	onConnect(user)
-}
+			}
+			return { contract, currentUser, nearConfig, walletConnection, near }
 
 		}
-		fetchData().then(() => {
-			console.log("fetch");
+		fetchData().then(async ({ contract, currentUser, nearConfig, walletConnection, near }) => {
+			setConnect({ contract, currentUser, nearConfig, walletConnection, near })
+			global.nearConnect = { contract, currentUser, nearConfig, walletConnection, near }
+
+			if (currentUser) {
+
+				// setUser(+currentUser.balance, currentUser.accountId)
+			}
+			return true
+		}
+		).then(async (res) => {
+			let promise = new Promise((resolve, reject) => {
+				setTimeout(() => resolve("get global"), 200)
+			});
+
+			let resultPromise = await promise;
+			if (resultPromise) {
+				let balance = global.nearConnect.currentUser.balance
+				let userId = global.nearConnect.currentUser.accountId.toString()
+				if (onConnect && balance && userId) {
+					onConnect(Number(balance), userId, "json")
+				}
+			}
 		})
 	}, [])
+
 	const signIn = async () => {
 		await connect.walletConnection.requestSignIn(
-		  connect.contract.accountId,
-		  null, //optional URL to redirect to if the sign in was successful
-		  null //optional URL to redirect to if the sign in was NOT successful
+			connect.contract.accountId,
+			null, //optional URL to redirect to if the sign in was successful
+			null //optional URL to redirect to if the sign in was NOT successful
 		);
 		console.log("in");
-		setAction(null)
-	  };
-	
-	  const signOut = () => {
-		  console.log("signOut",connect);
+	};
+
+	const signOut = () => {
+		console.log("signOut", connect);
 		connect.walletConnection.signOut();
 		window.location.replace(window.location.origin + window.location.pathname);
 		console.log("out");
-		onConnect(null,0)
-	  };
-	  
+		if (onConnect) {
+			onConnect(0, "")
+		}
+	};
 
-	if (connect !== null ) {
-		console.log("if connect0",connect);
-		// if (connect.currentUser){
-		// onConnect(connect.currentUser.balance, connect.currentUser.accountId )
-		// }
-
-	return(
-		<View>
-      {connect.currentUser? <Button  
-      icon={iconOut?iconOut:null}
-	  color={backgroundColor}
-      title={signOutTitle} 
-      onPress={signOut}></Button>:
-	  <Button  
-      icon={iconIn?iconIn:null}
-	  color={backgroundColor}
-      title={signInTitle} 
-      onPress={signIn}></Button>}
-    </View>
-  )
-} else {
-	return (
-		<p>"Loading"</p>
-	)
-}
+	if (connect) {
+		return (
+			<View>
+				{connect.currentUser ? <Button
+					icon={iconOut ? iconOut : null}
+					color={backgroundColor}
+					title={signOutTitle}
+					onPress={signOut}></Button> :
+					<Button
+						icon={iconIn ? iconIn : null}
+						color={backgroundColor}
+						title={signInTitle}
+						onPress={signIn}></Button>}
+			</View>
+		)
+	} else {
+		return (
+			<p>"Loading"</p>
+		)
+	}
 }
 
 
